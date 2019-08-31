b80417b481230e9c0e8c1f487cacde99bdaf1840
ProZhanng <prozhanng@gmail.com>
2019-08-31 17:01:18 +0800

    add mergeSort file

---
+++ algorithm/src/main/java/com/learn/sort/MergeSort.java
@@ -0,0 +1,10 @@
+package com.learn.sort;
+
+public class MergeSort implements Sort {
+
+    public void sort(int[] arr) {
+        if (arr==null||arr.length<2){
+            return;
+        }
+    }
+}
